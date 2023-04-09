b9f285bc970c99da88dfe8124f17d6304bd8b50e
dusk8u23 <97526600+dusk8u23@users.noreply.github.com>
2023-04-09 12:43:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '猪宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oESTu6kTyUKSpw_HlNlHB-vuh_4Q',
+      id: 'oESTu6kTCjIOYcK7k7Y47MSS8-D8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'H7d1v0jk8Q2Rv_SGqsGld6F6G0iES5I0r1c7tzT17iA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
